1ea7ed3b40ef84cca84b52612cf5bdecc3a34f8f
vdorchan <vdorchan@gmail.com>
2018-11-07 15:31:12 +0800

    fixed: fixed style syntactic error

--- template/src/App.vue
+++ template/src/App.vue
@@ -5,18 +5,15 @@
 </template>
 
 <style>
-  <% if(isRem) { %>
-  .#app {
+  <% if(isRem) { %>#app {
     width: 7.5rem;margin: 0px auto;
-  }
-  <% } else if(isWap) { %>#app{
+  }<% } else if(isWap) { %>#app{
     min-width:320px;
     margin:0 auto;
   }
   @media only screen and (min-width: 1025px) {
     #app{max-width:320px}
-  }
-  <% } else { %>body .g-footer .gft-links,
+  }<% } else { %>body .g-footer .gft-links,
   body .g-footer .gft-links a {
     color:#000;
   }
